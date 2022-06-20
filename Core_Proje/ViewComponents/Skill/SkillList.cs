9044beef80115c0f3aeabf156af6dff4b08ff871
Osman Ünüvar <osmanunuvar@gmail.com>
2022-06-20 08:24:11 +0300

    Add project files.

---
+++ Core_Proje/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Skill
+{
+    public class SkillList:ViewComponent
+    {
+        SkillManager SkillManager = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = SkillManager.TGetList();
+            return View(values);
+        }
+    }
+}
